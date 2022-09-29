1d3122328c82d7a56ff91f97a29fd638ffd2f6e7
Valya <benedict.1@yandex.ru>
2022-09-29 21:04:12 +0300

    Initial commit

---
+++ src/app/api/index.js
@@ -0,0 +1,7 @@
+import users from "./fake.api/user.api";
+import professions from "./fake.api/professional.api";
+const API = {
+    users,
+    professions
+};
+export default API;
---
+++ src/app/components/groupList.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const GroupList = ({
+    items,
+    valueProperty,
+    contentProperty,
+    onItemSelect,
+    selectedItem
+}) => {
+    let serializationItems = {};
+    if (typeof (items) === "object") {
+        serializationItems = items;
+    } else {
+        serializationItems = items.reduce((acc, a) => { acc[a[contentProperty]] = a; return acc; }, {});
+    }
+    return (
+        <>
+            <ul className="list-group">
+                {Object.keys(serializationItems).map((item) => (
+                    <li
+                        key={serializationItems[item][valueProperty]}
+                        className={
+                            "list-group-item" +
+                            (serializationItems[item] === selectedItem ? " active" : "")
+                        }
+                        onClick={() => onItemSelect(serializationItems[item])}
+                        role="button"
+                    >
+                        {serializationItems[item][contentProperty]}
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+};
+GroupList.defaultProps = {
+    valueProperty: "_id",
+    contentProperty: "name"
+};
+GroupList.propTypes = {
+    items: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+    valueProperty: PropTypes.string,
+    contentProperty: PropTypes.string,
+    onItemSelect: PropTypes.func,
+    selectedItem: PropTypes.object.isRequired
+};
+
+export default GroupList;
